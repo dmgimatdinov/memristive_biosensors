208b5a425061e37beca24cd6ffe769fc87b83a63
cozydnb <dmgimatdinov@gmail.com>
2025-08-04 18:26:36 +0300

    Добавление паспортов MEM.

--- DB_6.py
+++ DB_6.py
@@ -672,15 +672,15 @@ class BiosensorGUI:
                 'ph_max': {'min': 2.0, 'max': 10.0},
                 't_min': {'min': 5, 'max': 120},
                 't_max': {'min': 5, 'max': 120},
-                'dr_min': {'min': 0.01, 'max': 1000},
-                'dr_max': {'min': 0.01, 'max': 1000},
+                'dr_min': {'min': 0.0000001, 'max': 100000000000},
+                'dr_max': {'min': 0.0000001, 'max': 100000000000},
                 'young_modulus': {'min': 0, 'max': 1000},
                 'sensitivity': {'min': 0, 'max': 1000},
                 'reproducibility': {'min': 0, 'max': 100},
                 'response_time': {'min': 0, 'max': 3600},
                 'stability': {'min': 0, 'max': 365},
-                'lod': {'min': 0, 'max': 1000},
-                'durability': {'min': 0, 'max': 8760},
+                'lod': {'min': 0, 'max': 100000},
+                'durability': {'min': 0, 'max': 100000},
                 'power_consumption': {'min': 0, 'max': 1000}
             }
         }
@@ -711,12 +711,12 @@ class BiosensorGUI:
                 {'label': 'pH диапазон:', 'type': 'range', 'min_var': 'ph_min', 'max_var': 'ph_max', 'hint': '2.0 — 10.0'},
                 {'label': 'Температурный диапазон (°C):', 'type': 'range', 'min_var': 't_min', 'max_var': 't_max', 'hint': '2 — 120'},
                 {'label': 'Диапазон измерений (пМ):', 'type': 'range', 'min_var': 'dr_min', 'max_var': 'dr_max', 'hint': '0.1 — 1*10^12'},
-                {'label': 'Чувствительность (мкА/(мкМ*см^2)):', 'var_name': 'sensitivity', 'hint': '0.0001 — 2*10^4'},
+                {'label': 'Чувствительность (мкА/(мкМ*см^2)):', 'var_name': 'sensitivity', 'hint': '0.01 — 1000'},
                 {'label': 'Воспроизводимость (%):', 'var_name': 'reproducibility', 'hint': '0-100'},
                 {'label': 'Время отклика (с):', 'var_name': 'response_time', 'hint': '0-3600'},
                 {'label': 'Стабильность (дни):', 'var_name': 'stability', 'hint': '0-365'},
-                {'label': 'Предел обнаружения (нМ):', 'var_name': 'lod', 'hint': '0-50000'},
-                {'label': 'Долговечность (ч):', 'var_name': 'durability', 'hint': '0-8760'},
+                {'label': 'Предел обнаружения (нМ):', 'var_name': 'lod', 'hint': '0-100000'},
+                {'label': 'Долговечность (ч):', 'var_name': 'durability', 'hint': '0-100000'},
                 {'label': 'Энергопотребление (мВт):', 'var_name': 'power_consumption', 'hint': '0-1000'}
             ],
             'immobilization': [
@@ -739,13 +739,13 @@ class BiosensorGUI:
                 {'label': 'Название:', 'var_name': 'mem_name', 'hint': 'Тип мемристора'},
                 {'label': 'pH диапазон:', 'type': 'range', 'min_var': 'ph_min', 'max_var': 'ph_max', 'hint': '2.0 — 10.0'},
                 {'label': 'Температурный диапазон (°C):', 'type': 'range', 'min_var': 't_min', 'max_var': 't_max', 'hint': '5 — 100'},
-                {'label': 'Диапазон измерений:', 'type': 'range', 'min_var': 'dr_min', 'max_var': 'dr_max', 'hint': '0.01 — 1000'},
+                {'label': 'Диапазон измерений (пМ):', 'type': 'range', 'min_var': 'dr_min', 'max_var': 'dr_max', 'hint': '0.0000001 — 0-1*10^11'},
                 {'label': 'Модуль Юнга (ГПа):', 'var_name': 'young_modulus', 'hint': '0-1000'},
                 {'label': 'Чувствительность (мВ/dec):', 'var_name': 'sensitivity', 'hint': '0-1000'},
                 {'label': 'Воспроизводимость (%):', 'var_name': 'reproducibility', 'hint': '0-100'},
                 {'label': 'Время отклика (с):', 'var_name': 'response_time', 'hint': '0-3600'},
                 {'label': 'Стабильность (дни):', 'var_name': 'stability', 'hint': '0-365'},
-                {'label': 'Предел обнаружения:', 'var_name': 'lod', 'hint': '0-1000'},
+                {'label': 'Предел обнаружения (нМ):', 'var_name': 'lod', 'hint': '0-1*10^5'},
                 {'label': 'Долговечность (ч):', 'var_name': 'durability', 'hint': '0-8760'},
                 {'label': 'Энергопотребление (мВт):', 'var_name': 'power_consumption', 'hint': '0-1000'}
             ]
